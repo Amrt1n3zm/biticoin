ff45a8f5423f0365b1a6b2bdaa9a2fbca174c82f
Alejandro <10244154+Amrt1n3zm@users.noreply.github.com>
2022-04-06 04:48:03 +0200

    Update Crypto.java

--- Crypto.java
+++ Crypto.java
@@ -1,62 +1,12 @@
-package info.osom.sandbox;
-
-import javax.crypto.*;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-
 public class Crypto {
-    private static final String KEY = "12345678901234567890123456789012";
-    private static final String IV = "1234567890123456";
-    private static final String ENGINE = "AES";
-    private static final String CRYPTO = "AES/CBC/PKCS5Padding";
-    private static final int BUFFER_SIZE = 4 * 1024;
 
-    public static void encrypt(InputStream fis, OutputStream fos)
-            throws NoSuchPaddingException, BadPaddingException,
-            InvalidKeyException, NoSuchAlgorithmException,
-            IOException, ShortBufferException,
-            IllegalBlockSizeException, InvalidAlgorithmParameterException {
-        execute(Cipher.ENCRYPT_MODE, fis, fos);
-    }
-
-    public static void decrypt(InputStream fis, OutputStream fos)
-            throws NoSuchPaddingException, BadPaddingException,
-            InvalidKeyException, NoSuchAlgorithmException,
-            IOException, ShortBufferException,
-            IllegalBlockSizeException, InvalidAlgorithmParameterException {
-        execute(Cipher.DECRYPT_MODE, fis, fos);
-    }
-
-    private static void execute(int mode, InputStream fis, OutputStream fos)
-            throws NoSuchPaddingException, NoSuchAlgorithmException,
-            InvalidAlgorithmParameterException,
-            InvalidKeyException, IOException, ShortBufferException,
-            BadPaddingException, IllegalBlockSizeException
-    {
-        SecretKeySpec sks = new SecretKeySpec(KEY.getBytes(), ENGINE);
-        IvParameterSpec ivps = new IvParameterSpec(IV.getBytes());
-
-        Cipher cipher = Cipher.getInstance(CRYPTO);
-        cipher.init(mode, sks, ivps);
-
-        byte[] inBuffer = new byte[BUFFER_SIZE];
-        byte[] outBuffer = new byte[BUFFER_SIZE];
-        int inBytes;
-        int outBytes;
-
-        while ((inBytes = fis.read(inBuffer)) != -1) {
-            outBytes = cipher.update(inBuffer, 0, inBytes, outBuffer);
-            fos.write(outBuffer, 0, outBytes);
-        }
-
-        outBytes = cipher.doFinal(outBuffer, 0);
-        fos.write(outBuffer, 0, outBytes);
-        fos.flush();
+    /**
+     * @return true is {@code signature} is a valid digital signature of {@code message} under the
+     *         key {@code pubKey}. Internally, this uses RSA signature, but the student does not
+     *         have to deal with any of the implementation details of the specific signature
+     *         algorithm
+     */
+    public static boolean verifySignature(PublicKey pubKey, byte[] message, byte[] signature) {
+        ...
     }
 }
